9b9c20c219b8ed27f5c8c677d48be7acfb53583f
magdumrushikesh <rushikesh4525@gmail.com>
2025-01-08 15:19:56 +0530

    Adding Emplyee Assignment

---
+++ Day7 File handling/Day7 File Handling/Day7 File Handling/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day7_File_Handling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
