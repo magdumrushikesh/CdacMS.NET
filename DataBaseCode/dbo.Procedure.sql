7b63bff3142ff1c181aae1afbc188babbfc47698
magdumrushikesh <rushikesh4525@gmail.com>
2025-01-29 23:57:25 +0530

    Add db1

--- DataBaseCode/dbo.Procedure.sql
+++ DataBaseCode/dbo.Procedure.sql
@@ -1,8 +1,8 @@
-﻿CREATE PROCEDURE [dbo].[Procedure]
+﻿ CREATE PROCEDURE [dbo].[Procedure]
 	@EmpNo int ,
 	@Name varchar(50),
 	@Basic  decimal(18,2),
 	@DeptNo int
 AS
 	insert into Employees values(@EmpNo,@Name,@Basic,@DeptNo)
-RETURN 0 
+RETURN 0  
